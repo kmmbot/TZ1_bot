5ff240676517a6321b39d0aaae69002434677b0f
kmmbot <muradinkanukov@gmail.com>
2025-01-17 16:12:36 +0300

    Add files via upload
    бот с интегрированным биллингом

--- billing.py
+++ billing.py
@@ -1,23 +1,28 @@
+import requests
 import hmac
 import hashlib
-import requests
 
-url = "https://192.168.214.130/api/v1/billing/users/search"
-salt = "123456"
-secret_key = "d6538f988734083acdb091ca5d7f0c6992643458657a932f1dd0feb664ed239e0acb38aa58396074aeaa63e736f545e969687fa6c42e160c3e140e410a6fa12e"
+url = "http://192.168.214.133/api/v1/billing/users/search"
+salt = "12345"
+secret = "d6538f988734083acdb091ca5d7f0c6992643458657a932f1dd0feb664ed239e0acb38aa58396074aeaa63e736f545e969687fa6c42e160c3e140e410a6fa12e"
+sign = hmac.new(secret.encode(), salt.encode(), hashlib.sha512).hexdigest()
 
-sign = hmac.new(secret_key.encode(), salt.encode(), hashlib.sha512).hexdigest()
+payload = {
+    'field': 'numdogovor',
+    'operator': '=',
+    'value': '1234',
+    'sign': sign,
+    'salt': salt
+}
 
-payload={'field': 'uid',
-'operator': '=',
-'value': '7',
-'sign': sign,
-'salt': salt}
-files=[
-
-]
+# Заголовки запроса
 headers = {}
 
-response = requests.request("POST", url, headers=headers, data=payload, files=files, verify=False)
+# Выполняем запрос
+response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+data = response.json()
+
+if data['success'] and 'data' in data and len(data['data']) > 0:
+    phone_number = data['data'][0]['address']
+    print(data)
